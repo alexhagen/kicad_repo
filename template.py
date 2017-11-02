24e61e18ad834520046b641ce6b9375ba0d70a0f
Alex Hagen <ahagen@purdue.edu>
2017-11-02 09:02:25 -0400

    pseudocoding

---
+++ template.py
@@ -0,0 +1,7 @@
+# currently pseudocode for how to template a kicad_repo circuit project
+
+# first, make the folder that this will all go in
+# then, enter that folder
+# make a folder for any code named 'repo/code'
+# make a folder for any sim named 'repo/sim'
+# make a folder for the circuit named 'repo/circuit'
